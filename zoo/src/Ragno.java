3f4d0578d6da4860e84064c4a23ccbd6745897fe
premaor <131602440+canioest@users.noreply.github.com>
2023-11-29 16:36:33 +0100

    Update Ragno.java

--- zoo/src/Ragno.java
+++ zoo/src/Ragno.java
@@ -1,1 +1,14 @@
-
+public class Ragno extends Invertebrati {
+     public Polpo(String s, int zampe, float p, int l) {
+          nome=s;
+          nZampe=zampe;
+          peso=p;
+          altezza=l;
+          alimentazione="Carnivoro";
+          isDiurno=false;
+          isAllatta=false;
+          isPartorisce=false;
+          isOviperi=true;
+          abitat="Boschi";
+     }
+}
